fa84baea20cae6705b0feeb323f123b82e636aad
Bram Kreuger <bramkreuger@gmail.com>
2020-04-14 11:39:24 +0200

    ..

---
+++ docs/scripts/homepage.js
@@ -0,0 +1,17 @@
+var which_text_box;
+which_text_box = 0;
+
+switch (which_text_box){
+    case 0: //none
+
+        break;
+    case:1 //vr-box
+
+        break;
+    case 2: //over ons
+
+        break;
+    case 3:
+
+        break;
+}
